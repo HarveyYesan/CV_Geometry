e58a2b8a6c537a1ad591dbc2aba52f944fb573fe
HarveyYesan <yuhaiyong@foxmail.com>
2018-06-30 21:09:02 +0800

    stitch point cloud based on pcl
    * according to BRG images & depth
    
    Signed-off-by: HarveyYesan <yuhaiyong@foxmail.com>

---
+++ point_cloud_stitching/build/CMakeFiles/joinMap.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/harvey/Workspace/CV_Geometry/point_cloud_stitching/src/joinMap.cpp" "/home/harvey/Workspace/CV_Geometry/point_cloud_stitching/build/CMakeFiles/joinMap.dir/src/joinMap.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "qh_QHpointer"
+  "vtkDomainsChemistry_AUTOINIT=1(vtkDomainsChemistryOpenGL2)"
+  "vtkIOExport_AUTOINIT=1(vtkIOExportOpenGL2)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  "vtkRenderingOpenGL2_AUTOINIT=1(vtkRenderingGL2PSOpenGL2)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/usr/local/include/eigen3"
+  "/usr/local/include/vtk-8.1"
+  "/usr/local/include/pcl-1.8"
+  "/usr/local/pcl/visualization/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
